8908378830cc3594f67c15ad56b927c25534166e
coronafromchina <69694964+coronafromchina@users.noreply.github.com>
2025-11-10 00:26:11 +0800

    testing white screen issue

--- assets/setup/env.php
+++ assets/setup/env.php
@@ -1,14 +1,14 @@
 <?php
 
 if (!defined('APP_NAME'))                       define('APP_NAME', 'Login System');
-if (!defined('APP_ORGANIZATION'))               define('APP_ORGANIZATION', 'KLiK');
-if (!defined('APP_OWNER'))                      define('APP_OWNER', 'msaad1999');
+if (!defined('APP_ORGANIZATION'))               define('APP_ORGANIZATION', '');
+if (!defined('APP_OWNER'))                      define('APP_OWNER', '');
 if (!defined('APP_DESCRIPTION'))                define('APP_DESCRIPTION', 'Embeddable PHP Login System');
 
 if (!defined('ALLOWED_INACTIVITY_TIME'))        define('ALLOWED_INACTIVITY_TIME', time()+1*60);
 
-if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'klik_loginsystem');
-if (!defined('DB_HOST'))                        define('DB_HOST','127.0.0.1');
+if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'localhost');
+if (!defined('DB_HOST'))                        define('DB_HOST','localhost');
 if (!defined('DB_USERNAME'))                    define('DB_USERNAME','root');
 if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD' ,'');
 if (!defined('DB_PORT'))                        define('DB_PORT' ,'');
